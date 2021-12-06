49f5ceee2584c0c8d931ae30acabfd5cbd8efd5c
MateoCh <53842157+MateoCh@users.noreply.github.com>
2021-12-06 15:42:07 -0500

    Corrección ids y valores devices

--- p2web/src/components/device.js
+++ p2web/src/components/device.js
@@ -4,10 +4,10 @@ function Device(props)
 {
     return(
         <tr>
-            <th scope="row">{props.ind}</th>
+            <th scope="row">{props.ind+1}</th>
             <td>{props.dev.id}</td>
             <td>{props.dev.name}</td>
-            <td>{props.dev.desired.value}</td>
+            <td>{""+props.dev.desired.value}</td>
         </tr>
     );
 }
